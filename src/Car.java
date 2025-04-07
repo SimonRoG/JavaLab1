27f02ea3282f650b53349a84636b9372c2fbe42e
Semen Prokhoda <prokhodasemen@gmail.com>
2025-04-07 23:27:03 +0200

    added some methods

--- src/Car.java
+++ src/Car.java
@@ -1,21 +1,41 @@
 public class Car {
+    private static int nextId = 0;
+    private int id;
     private String brand;
     private String model;
-    int hp;
-    boolean turboMod;
+    private int hp;
+    private int weight;
+    private boolean turboMod;
 
     public Car() {
+        id = nextId++;
         brand = "Porsche";
         model = "911 Dakar";
         hp = 480;
+        weight = 1680;
         turboMod = false;
     }
 
-    public Car(String brand, String model, int hp, boolean turboMod) {
+    public Car(String brand, String model) {
+        id = nextId++;
+        this.brand = brand;
+        this.model = model;
+        hp = 200;
+        weight = 1000;
+        turboMod = false;
+    }
+
+    public Car(String brand, String model, int hp, int weight, boolean turboMod) {
+        id = nextId++;
         this.brand = brand;
         this.model = model;
         this.hp = hp;
+        this.weight = weight;
         this.turboMod = turboMod;
+    }
+
+    public int getId() {
+        return id;
     }
 
     public String getName() {
@@ -23,20 +43,43 @@ public class Car {
     }
 
     public void info() {
-        System.out.printf("%s %s has %s HP. %s modified.%n", brand, model, hp, turboMod ? "Turbo" : "Not" );
+        System.out.printf("%s has %s HP. Weighs %s. %s modified.%n", this.getName(), hp, weight, turboMod ? "Turbo" : "Not" );
     }
 
     public void modify() {
         if (turboMod) {
-            System.out.printf("%s %s is already modified.%n", brand, model);
+            System.out.printf("%s is already modified.%n", this.getName());
         } else {
             turboMod = true;
             hp += 0.3 * hp;
-            System.out.printf("%s %s modified.%n", brand, model);
+            weight += 20;
+            System.out.printf("%s modified.%n", this.getName());
+        }
+    }
+
+    public void modify(int turbines) {
+        if (turboMod) {
+            System.out.printf("%s is already modified.%n", this.getName());
+        } else if (turbines > 4) {
+            System.out.printf("%s is too many for modification of %s.%n", turbines, this.getName());
+        } else if (turbines < 1) {
+            System.out.printf("there must be at least 1 turbine to modify %s.%n", this.getName());
+        } else {
+            turboMod = true;
+            hp += hp * (0.1 + 0.2 * turbines);
+            weight += 20 * turbines;
+            System.out.printf("%s modified.%n", this.getName());
         }
     }
 
     public boolean isMorePowerful(Car car) {
-        return this.hp > car.hp;
+        return (this.hp > car.hp);
+    }
+
+    public static Car builtAMonster(Car car1, Car car2, String monsterModel) {
+        int monsterHP = (int) ((car1.hp + car2.hp) * 0.9);
+        int monsterWeight = car1.weight + car2.weight;
+        boolean monsterTurbo = car1.turboMod || car2.turboMod;
+        return new Car("Monster", monsterModel, monsterHP, monsterWeight, monsterTurbo);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,19 +4,40 @@ public class Main {
     public static void main(String[] args) {
         ArrayList<Car> carList = new ArrayList<>();
 
+//        Car car = null;
+//        car.brand = "VW";
+//        car.model = "Polo";
+//        car.hp = 125;
+//        car.weight = 1250;
+//        car.turboMod = false;
+
         carList.add(new Car());
-        carList.add(new Car("Königsegg", "CC850", 1385, true));
+        carList.add(new Car("Semen-Mobil", "2004"));
+        carList.add(new Car("Königsegg", "CC850", 1385, 1385, true));
+
+        Car smallMonster = Car.builtAMonster(carList.getFirst(), carList.get(1), "SP1");
+        carList.add(smallMonster);
+
+        carList.get(1).info();
+        carList.get(1).modify();
+        System.out.println();
 
         for (Car car : carList) {
-            car.info();
-            car.modify();
             car.info();
             System.out.println();
         }
 
-        System.out.printf("%s is %s powerful then %s%n",
+        System.out.printf("%s is %s powerful then %s%n%n",
                 carList.getFirst().getName(),
-                carList.getFirst().isMorePowerful(carList.getLast()) ? "more" : "less",
-                carList.getLast().getName());
+                carList.getFirst().isMorePowerful(carList.get(1)) ? "more" : "less",
+                carList.get(1).getName());
+
+        Car monster = Car.builtAMonster(carList.getFirst(), carList.get(2), "SP2");
+        carList.add(monster);
+        monster.info();
+        System.out.println();
+
+        carList.get(smallMonster.getId()).modify(4);
+        carList.get(smallMonster.getId()).info();
     }
 }
