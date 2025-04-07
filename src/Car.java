22634f0f67d35533576185035b51a775e943d9b0
Semen Prokhoda <prokhodasemen@gmail.com>
2025-04-07 16:40:10 +0200

    car class modified

--- src/Car.java
+++ src/Car.java
@@ -1,21 +1,42 @@
 public class Car {
-    String brand;
-    String model;
-    int horsePower;
+    private String brand;
+    private String model;
+    int hp;
+    boolean turboMod;
 
     public Car() {
         brand = "Porsche";
         model = "911 Dakar";
-        horsePower = 480;
+        hp = 480;
+        turboMod = false;
     }
 
-    public Car(String brand, String model, int horsePower) {
+    public Car(String brand, String model, int hp, boolean turboMod) {
         this.brand = brand;
         this.model = model;
-        this.horsePower = horsePower;
+        this.hp = hp;
+        this.turboMod = turboMod;
+    }
+
+    public String getName() {
+        return brand + " " + model;
     }
 
     public void info() {
-        System.out.printf("%s %s has %s HP%n", brand, model, horsePower);
+        System.out.printf("%s %s has %s HP. %s modified.%n", brand, model, hp, turboMod ? "Turbo" : "Not" );
+    }
+
+    public void modify() {
+        if (turboMod) {
+            System.out.printf("%s %s is already modified.%n", brand, model);
+        } else {
+            turboMod = true;
+            hp += 0.3 * hp;
+            System.out.printf("%s %s modified.%n", brand, model);
+        }
+    }
+
+    public boolean isMorePowerful(Car car) {
+        return this.hp > car.hp;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,22 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        Car car = new Car();
-        car.info();
-        Car Koenigsegg = new Car("Königsegg", "CC850", 1385);
-        Koenigsegg.info();
+        ArrayList<Car> carList = new ArrayList<>();
+
+        carList.add(new Car());
+        carList.add(new Car("Königsegg", "CC850", 1385, true));
+
+        for (Car car : carList) {
+            car.info();
+            car.modify();
+            car.info();
+            System.out.println();
+        }
+
+        System.out.printf("%s is %s powerful then %s%n",
+                carList.getFirst().getName(),
+                carList.getFirst().isMorePowerful(carList.getLast()) ? "more" : "less",
+                carList.getLast().getName());
     }
 }
